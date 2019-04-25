694b9a210511a155d4b6c09073e859f43067d513
Martin Hamon <martinh@outlook.fr>
2019-04-25 13:10:09 +0200

    espace admin supp et modif

--- src/AdminCard.jsx
+++ src/AdminCard.jsx
@@ -1,52 +1,60 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { Card, CardImg, CardTitle, Button, Row, Col } from 'reactstrap';
 import './AdminCard.css'
-import { Card, CardImg,CardTitle, Button, Row, Col } from 'reactstrap';
 
 
 class AdminCard extends Component {
-    deleteWine(id){
-        const config ={
+
+    updateWine(id) {
+        this.props.history.push(`/adminForm/${id}`);
+    }
+
+    deleteWine(id) {
+        const config = {
             method: "DELETE",
-        } 
+        }
 
         const url = "http://51.68.18.101:3002/vinoveritas/api/wines/" + id;
 
+        fetch(url, config)
+            .then(res => {
+                if (res.ok) {
+                    alert("Vin supprimé");
+                } else {
+                    alert("Vin introuvable");
+                }
+            })
+            .catch(err => console.error(err))
+    }
 
-        fetch(url,config)
-        .then(res => {
-            (res.OK)? alert("Vin introuvable") : alert("Vin supprimé");
-            
-        })
-        .catch(err => console.error(err))
-      }   
+    render() {
+        const { image, title, id } = this.props.wine
+        return (
+            <div className="AdminCard col-sm-3">
+                <Card body>
+                    <Row>
+                        <Col sm="12" className="imageCard">
+                            <CardImg src={image} />
+                        </Col>
+                    </Row>
 
-    render(){
-    const {image,title,id} = this.props.wine
-    return (
-       
-            <Col sm="2" className="AdminCard">
-            <Card body>
-            <Row>
-                <Col sm="12" className="imageCard">
-                    <CardImg src={image}/>
-                </Col>
-            </Row>
-            
-                <CardTitle className="title">{title}</CardTitle>
-                <Row>
-                    <Col>                  
-                        <Button>Modifier</Button>
-                    </Col>
-                    <Col>
-                        <Button onClick={() => {this.deleteWine(id); }}>Supprimer</Button>
-                    </Col>
-                </Row>
-            </Card>
-            
-            </Col>
+                    <CardTitle className="title">{title}</CardTitle>
+                    <Row>
 
+                        <Col>
+                            <Button onClick={() => { this.updateWine(id) }}>Modif vin</Button>
+                        </Col>
 
-    );
-};}
+                        <Col>
+                            <Button onClick={() => { this.deleteWine(id); }}>Supprimer</Button>
+                        </Col>
+                    </Row>
+                </Card>
 
-export default AdminCard;
+            </div>
+        );
+    };
+}
+
+export default withRouter(AdminCard);
